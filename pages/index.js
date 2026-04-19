ec2a4d8ba73047cb5706fa0252c99bfdc9e868d4
soydanijuarez <juarezmadaniela@gmail.com>
2026-04-19 11:27:25 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: "sans-serif", textAlign: "center", padding: 40 }}>
+      <h1>🎓 MateJuegos</h1>
+      <p>La plataforma de matemática de Dani</p>
+      <p>¡Próximamente los juegos!</p>
+    </div>
+  );
+}
